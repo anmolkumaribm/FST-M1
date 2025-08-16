adf7ef7c2404cdde9f37be1a50020403694f489a
anmolkumaribm <anmol.kumar2@ibm.com>
2025-08-16 21:14:53 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,38 @@
+package Activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new FirefoxDriver();
+		driver.get("https://training-support.net");
+	}
+	
+	@Test(priority = 1)
+	public void homePageTest() {
+		Assert.assertEquals(driver.getTitle(), "Training Support");
+	}
+	
+	
+	@Test (priority = 2)
+	public void aboutPageTest() {
+		driver.findElement(By.linkText("About Us")).click();
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
