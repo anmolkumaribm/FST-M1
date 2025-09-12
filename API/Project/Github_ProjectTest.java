488eca4ea2d65828f64c413fe482d2f45a8cd65e
anmolkumaribm <anmol.kumar2@ibm.com>
2025-09-12 15:17:32 +0530

    Add files via upload

--- API/Project/Github_ProjectTest.java
+++ API/Project/Github_ProjectTest.java
@@ -27,47 +27,54 @@ public class GitHubProjectTest {
 
 	@BeforeClass
 	public void setUp() {
-		// Initialize the request Specification
-		requestSpec = new RequestSpecBuilder().setBaseUri("https://api.github.com/user/keys")
-				.addHeader("Content-Type", "application/json")
-				.addHeader("Authorization", "token")
-				.addHeader("X-GitHub-Api-Version", "2022-11-28"). // this is custom header
-				build();
-
-		// Initialize the response Specification
-		responseSpec = new ResponseSpecBuilder().expectBody("title", Matchers.equalTo("TestKeys"))
-				.expectBody("key", Matchers.equalTo(sshkey)).expectResponseTime(Matchers.lessThanOrEqualTo(3000L))
-				.build();
-
+		  // Ini the request specification
+		//can be multiple spec as well //but should be a productive code
+	        requestSpec = new RequestSpecBuilder().
+	        setBaseUri("https://api.github.com/user/keys").
+	        addHeader("Content-Type", "application/json").
+	        addHeader("Authorization", "token ************************"). 
+	        addHeader("X-GitHub-Api-Version", "2022-11-28").
+	        //can be other as  well like report etc 
+	        build();
+	    // Ini the response specification
+	        responseSpec = new ResponseSpecBuilder().
+	        		expectBody("title",Matchers.equalTo("TestKey")).
+	        	    expectBody("key", Matchers.equalTo(sshkey)).
+	        	    expectResponseTime(Matchers.lessThanOrEqualTo(3000L)).
+	        		build();
+	        
 	}
-
-	@Test(priority = 1)
-	public void postRequest() {
-		// Create Request Body
+	
+	@Test(priority=1)
+	public void postRequestTest() {
+		//create request body *mandate for post
 		HashMap<String, String> reqBody = new HashMap<String, String>();
-		reqBody.put("title", "TestKeys");
-		reqBody.put("key", sshkey);
-		// send request , save response
-		Response response = RestAssured.given().spec(requestSpec).body(reqBody).log().all().when().post();
-		// Extract the Id from Response
-		keyId = response.then().extract().path("id");
-		// Assertions
-		response.then().statusCode(201).spec(responseSpec).log().all();
-
+        reqBody.put("title", "TestKey");
+        reqBody.put("key", sshkey);
+        
+		//send request , save response
+        Response response = RestAssured.given().
+        		spec(requestSpec).
+        		body(reqBody).log().all().
+        	when().
+        	post();  	
+      //Extract the id from response
+    	KeyId = response.then().extract().path("id"); // dont use dollar same as JSON path
+     //Assertions
+    	response.then().statusCode(201).spec(responseSpec).log().all();
 	}
-
-	@Test(priority = 2)
+	
+	@Test(priority=2)
 	public void getRequestTest() {
-		// Send request, save response
-		RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).when().get("/{keyId}").then().statusCode(200)
-				.spec(responseSpec);
+		
+	    RestAssured.given().spec(requestSpec).pathParam("keyId", KeyId).when().get("/{keyId}").then().
+	    statusCode(200).spec(responseSpec).log().all();
 	}
-
-	@Test(priority = 3)
-	public void getDeleteTest() {
-		// Send request, save response
-		RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).when().delete("/{keyId}").then().statusCode(204)
-				.time(Matchers.lessThanOrEqualTo(3000L));
-	}
-
+	
+	@Test(priority=3)
+	public void deleteRequestTest() {
+		
+	    RestAssured.given().spec(requestSpec).pathParam("keyId", KeyId).when().delete("/{keyId}").then().
+	    statusCode(204).time(Matchers.lessThanOrEqualTo(3000L)).log().all();
+	}	
 }
